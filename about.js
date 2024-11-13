af5d0c3362046829764fd2f47a39b2d7c3eba677
Darshan <mwendwadaniel185@gmail.com>
2024-11-13 11:39:47 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,9 @@
+// Toggle the dropdown content visibility
+function toggleDropdown(id) {
+    const element = document.getElementById(id);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
